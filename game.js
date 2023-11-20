bcc628aeb7bf00448844dbe9a8f5bb215aa363ed
karishmaaaaa <karishma.ss@kalvium.community>
2023-11-20 10:36:35 +0530

    final

--- game.js
+++ game.js
@@ -1,5 +1,77 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+var num_1 = Math.round(Math.random()*100);
+var num2 = Math.round(Math.random()*100);
 
+var num_1div = document.getElementById("number1")
+num_1div.innerHTML = num_1;
+var num2div = document.getElementById("number2")
+num2div.innerHTML = num2;
 // Iteration 3: Make the options button functional
+var greater_button = document.getElementById("greater-than");
+var lesser_button = document.getElementById("lesser-than");
+var equalButton = document.getElementById("equal-to");
 
+var totalscore = 0;
+greater_button.onclick = () =>{
+    if (num_1 > num2){
+        totalscore++;
+        resetTimer(timerID)
+    }else{
+        window.location.href = "./gameover.html"
+    }
+
+    num_1 = Math.round(Math.random()*100);
+    num2 = Math.round(Math.random()*100);
+    num_1div.innerHTML = num_1;
+    num2div.innerHTML = num2;
+}
+
+equalButton.onclick = () =>{
+    if (num_1 == num2){
+        totalscore++;
+        resetTimer(timerID)
+    }else{
+        window.location.href = "./gameover.html"
+    }
+
+    num_1 = Math.round(Math.random()*100);
+    num2 = Math.round(Math.random()*100);
+    num_1div.innerHTML = num_1;
+    num2div.innerHTML = num2;
+}
+
+lesser_button.onclick = () =>{
+    if (num_1 < num2){
+        totalscore++;
+        resetTimer(timerID)
+    }else{
+        window.location.href = "./gameover.html"
+    }
+
+    num_1 = Math.round(Math.random()*100);
+    num2 = Math.round(Math.random()*100);
+    num_1div.innerHTML = num_1;
+    num2div.innerHTML = num2;
+}
 // Iteration 4: Build a timer for the game
+var present_time = document.getElementById('timer');
+var time = 5;
+var timerID;
+function startTimer(){
+    time = 5;
+    present_time.innerHTML = time;
+    timerID=setInterval(()=>{
+        time--;
+        if(time<=0){
+            window.location.href = "./gameover.html";
+        }
+        present_time.innerHTML= time;
+    }, 1000);
+    localStorage.setItem("score", JSON.stringify(totalscore));
+}
+function resetTimer(timerID){
+    console.log(timerID)
+    clearInterval(timerID)
+    startTimer();
+}
+startTimer();
--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var total = localStorage.getItem("score");
+
+var scoreBoard = document.getElementById("score-board")
+scoreBoard.innerHTML = total
+
+document.getElementById("play-again-button").addEventListener("click", function (){
+    window.location.href="./game.html";
+})
